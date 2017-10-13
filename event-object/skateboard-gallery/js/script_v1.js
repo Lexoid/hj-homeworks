240eb22ab60fceb21f37fac92fdfe922099ce93a
Lexoid <79522729759@yandex.ru>
2017-10-13 18:29:37 +0300

    skateboard-gallery
    update

--- event-object/skateboard-gallery/js/script_v1.js
+++ event-object/skateboard-gallery/js/script_v1.js
@@ -1,22 +1,17 @@
 const allBoard = document.querySelectorAll('.gallery-nav a');
 const imgActive = document.querySelector('.gallery-view');
-const allBoardTitle = document.querySelectorAll('.gallery-nav a img');
-const h2 = document.getElementsByTagName('h2');
 
 for (var i = 0; i < allBoard.length; i++) {
   allBoard[i].addEventListener('click', function (element) {
-    select(element);  
-  });  
-  allBoard[i].title = allBoardTitle[i].title;
+      select(element);
+  });
 }
 
 function select(element) {
   element.preventDefault();
   for (let i = 0; i < allBoard.length; i++) {
-    allBoard[i].classList.remove('gallery-current');
+      allBoard[i].classList.remove('gallery-current');
   }
-  
-  element.currentTarget.classList.add('gallery-current');  
-  h2[0].innerHTML = element.currentTarget.title;  
+  element.currentTarget.classList.add('gallery-current');
   imgActive.src = element.currentTarget.href;    
 }
