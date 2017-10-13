589691f53e91becf6f90277a3521e11d30327793
Lexoid <79522729759@yandex.ru>
2017-10-13 14:39:32 +0300

    edit

--- event-object/skateboard-gallery/js/script.js
+++ event-object/skateboard-gallery/js/script.js
@@ -2,16 +2,16 @@ const allBoard = document.querySelectorAll('.gallery-nav a');
 const imgActive = document.querySelector('.gallery-view');
 
 for (var i = 0; i < allBoard.length; i++) {
-    allBoard[i].addEventListener('click', function (element) {
-        select(element);
-    });
+  allBoard[i].addEventListener('click', function (element) {
+      select(element);
+  });
 }
 
 function select(element) {
-    element.preventDefault();
-    for (let i = 0; i < allBoard.length; i++) {
-        allBoard[i].classList.remove('gallery-current');
-    }
-    element.currentTarget.classList.add('gallery-current');
-    imgActive.src = element.currentTarget.href;    
+  element.preventDefault();
+  for (let i = 0; i < allBoard.length; i++) {
+      allBoard[i].classList.remove('gallery-current');
+  }
+  element.currentTarget.classList.add('gallery-current');
+  imgActive.src = element.currentTarget.href;    
 }
